4cb66deb43f3e233c85084c578f15ffd56ce26f3
LamKhanhDepzai <lamhuukhanh69@gmail.com>
2025-10-09 22:05:50 +0700

    fix port

--- hello.js
+++ hello.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
-
-
