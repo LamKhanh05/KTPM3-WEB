21875f115b17cb9bfe81c5fef4f7067f3c8f09e6
LamKhanhDepzai <lamhuukhanh69@gmail.com>
2025-10-09 22:17:25 +0700

    last update

--- hello.js
+++ hello.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.send('Hello World');
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
+
+
