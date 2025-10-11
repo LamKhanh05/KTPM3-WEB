b886f8a1d5819dfce0c19e870ca52ddd81c329d1
LamKhanhDepzai <lamhuukhanh69@gmail.com>
2025-10-11 20:26:45 +0700

    fix

--- hello.js
+++ hello.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
 });
