8810699c7f3fca06d70a06202e13c1297cf31a51
LamKhanhDepzai <lamhuukhanh69@gmail.com>
2025-10-11 20:18:06 +0700

    fix port binding for render/railway

--- hello.js
+++ hello.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
 });
 
 if (require.main === module) {
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 8080;
     app.listen(port, () => {
         console.log(`Server is running on http://localhost:${port}`);
     });
